91ddbcbaf8a4c3588de9d4d34f605b807a560188
Murat Benli <muratbenli001@gmail.com>
2019-11-06 20:29:13 +0300

    PostsController created and post editor added. Post editor's  Turkish language pack uploaded.

--- ButikBlog/Areas/Admin/Controllers/PostsController.cs
+++ ButikBlog/Areas/Admin/Controllers/PostsController.cs
@@ -6,12 +6,29 @@ using System.Web.Mvc;
 
 namespace ButikBlog.Areas.Admin.Controllers
 {
-    public class PostsController : Controller
+    public class PostsController : AdminBaseController
     {
         // GET: Admin/Posts
         public ActionResult Index()
         {
-            return View();
+            return View(db.Posts.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+
+            return Json(new { success = true });
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "Id", "CategoryName");
+            return View(db.Posts.Find(id));
         }
     }
 }
