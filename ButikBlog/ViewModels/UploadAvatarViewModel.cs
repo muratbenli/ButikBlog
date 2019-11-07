ffc560d598627f5669fa6d28a777e3e1a5105e3a
Murat Benli <muratbenli001@gmail.com>
2019-11-07 21:33:29 +0300

    UploadPhoto attribute added.

--- ButikBlog/ViewModels/UploadAvatarViewModel.cs
+++ ButikBlog/ViewModels/UploadAvatarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ButikBlog.Attributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,11 +7,12 @@ using System.Web;
 
 namespace ButikBlog.ViewModels
 {
-    public class UploadAvatarViewModel
+    public class UploadAvatarViewModel 
     {
         public string Photo { get; set; }
 
         [Required(ErrorMessage ="Resim dosyası seçmediniz.")]
+        [ProfilePhoto(MaxFileSize = 1000000,ErrorMessage ="1 Mb'den küçük bir resim giriniz.")]
         public HttpPostedFileBase File { get; set; }
     }
 }
